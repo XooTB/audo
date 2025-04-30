6feff9167cdf4c4a6176f1776ac0bf1823343fe8
Samiul Alim <xo.ot2001@gmail.com>
2025-05-01 00:52:26 +0600

    updated database tables to include chapters

---
+++ src-tauri/src/database/sqlite/migrations/src/m20250430_183042_update_books_table.rs
@@ -0,0 +1,27 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(DeriveIden)]
+enum Audiobook {
+    Table,
+    Id,
+    Title,
+    Author,
+    Chapters,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Audiobook::Table)
+                    .add_column(string(Audiobook::Chapters))
+                    .to_owned(),
+            )
+            .await
+    }
+}
