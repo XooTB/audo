b71b872a243638e396d06ef0bcac7833c8da7052
Samiul Alim <xo.ot2001@gmail.com>
2025-05-05 23:58:16 +0600

    feat: added db_checks before book_imports

--- src-tauri/src/book_managers/import_book.rs
+++ src-tauri/src/book_managers/import_book.rs
@@ -3,6 +3,7 @@ use crate::audio_lib::init;
 use crate::audio_lib::metadata;
 
 use crate::database::sqlite::entity::audiobook;
+use crate::database::sqlite::entity::audiobook::Entity as AudioBook;
 use crate::database::sqlite::Db;
 use sea_orm::*;
 
@@ -15,21 +16,39 @@ pub async fn import_book(file_path: &str, db: tauri::State<'_, Db>) -> Result<()
     let chapters = chapters::get_chapters(&context);
     let conn = &*db.inner().0;
 
-    let stringified_chapters = serde_json::to_string(&chapters).unwrap();
+    let exists = AudioBook::find()
+        .filter(audiobook::Column::Title.contains(&book_metadata.title))
+        .all(conn)
+        .await;
 
-    let new_book = audiobook::ActiveModel {
-        title: Set(book_metadata.title.to_owned()),
-        author: Set(book_metadata.author.to_owned()),
-        chapters: Set(stringified_chapters),
-        narrator: Set(book_metadata.narrator.to_owned()),
-        series: Set(book_metadata.series.to_owned()),
-        description: Set(book_metadata.description.to_owned()),
-        ..Default::default()
-    };
+    match exists {
+        Ok(e) => {
+            if e.len() >= 1 {
+                println!("Book already exists!");
+                Ok(())
+            } else {
+                let stringified_chapters = serde_json::to_string(&chapters).unwrap();
 
-    let _book_model = new_book.insert(conn).await.map_err(|e| e.to_string())?;
+                let new_book = audiobook::ActiveModel {
+                    title: Set(book_metadata.title.to_owned()),
+                    author: Set(book_metadata.author.to_owned()),
+                    chapters: Set(stringified_chapters),
+                    narrator: Set(book_metadata.narrator.to_owned()),
+                    series: Set(book_metadata.series.to_owned()),
+                    description: Set(book_metadata.description.to_owned()),
+                    ..Default::default()
+                };
 
-    println!("Book Imported successfull!");
+                let _ = new_book.insert(conn).await.map_err(|e| e.to_string())?;
 
-    Ok(())
+                println!("Book Imported successfull!");
+
+                Ok(())
+            }
+        }
+        Err(err) => {
+            println!("Something went wrong while querying the local database! Please try again!");
+            Err(err.to_string())
+        }
+    }
 }
