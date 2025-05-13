ff53d7dff3cc77ee970f37ffc7d036d5047b8647
Samiul Alim <xo.ot2001@gmail.com>
2025-05-14 00:14:34 +0600

    feat: added file_path to the migrations

--- src-tauri/src/database/sqlite/migrations/src/lib.rs
+++ src-tauri/src/database/sqlite/migrations/src/lib.rs
@@ -5,6 +5,7 @@ mod m20250430_183042_update_books_table;
 mod m20250502_045727_metadata_narrator;
 mod m20250502_045915_metadata_series;
 mod m20250502_050036_metadata_description;
+mod m20250513_180706_file_location;
 
 pub struct Migrator;
 
@@ -17,6 +18,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250502_045727_metadata_narrator::Migration),
             Box::new(m20250502_045915_metadata_series::Migration),
             Box::new(m20250502_050036_metadata_description::Migration),
+            Box::new(m20250513_180706_file_location::Migration),
         ]
     }
 }
