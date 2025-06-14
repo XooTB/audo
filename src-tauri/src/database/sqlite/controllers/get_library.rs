2ff50b624607bace2a327c37f9d235d19bec9aa5
XooTB <xo.ot2001@gmail.com>
2025-06-14 11:53:10 +0600

    fix: fixed get_library properties

--- src-tauri/src/database/sqlite/controllers/get_library.rs
+++ src-tauri/src/database/sqlite/controllers/get_library.rs
@@ -1,27 +1,27 @@
 use crate::database::sqlite::entity::audiobook;
 use crate::database::sqlite::entity::audiobook::Entity as AudioBook;
 use crate::database::sqlite::Db;
-use sea_orm::EntityTrait;
+use sea_orm::{EntityTrait, FromQueryResult, QuerySelect};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+// AudioBook Response struct
+#[derive(Serialize, Deserialize, Debug, FromQueryResult)]
 pub struct AudioBookResponse {
     pub id: i32,
     pub title: String,
     pub author: String,
-    pub chapters: String,
     pub narrator: String,
     pub series: String,
     pub description: String,
 }
 
+// Audiobook Response Implementation
 impl From<audiobook::Model> for AudioBookResponse {
     fn from(book: audiobook::Model) -> Self {
         Self {
             id: book.id,
             title: book.title,
             author: book.author,
-            chapters: book.chapters,
             narrator: book.narrator,
             series: book.series,
             description: book.description,
@@ -29,10 +29,25 @@ impl From<audiobook::Model> for AudioBookResponse {
     }
 }
 
+// get_library function
 #[tauri::command]
 pub async fn get_library(db: tauri::State<'_, Db>) -> Result<Vec<AudioBookResponse>, String> {
     let conn = &*db.inner().0;
-    let result = AudioBook::find().all(conn).await;
+    let result: Result<Vec<AudioBookResponse>, sea_orm::DbErr> = AudioBook::find()
+        .select_only()
+        .columns([
+            audiobook::Column::Id,
+            audiobook::Column::Title,
+            audiobook::Column::Series,
+            audiobook::Column::Author,
+            audiobook::Column::Narrator,
+            audiobook::Column::Description,
+        ])
+        .into_model::<AudioBookResponse>()
+        .all(conn)
+        .await;
+
+    println!("{:?}", result);
 
     match result {
         Ok(books) => Ok(books.into_iter().map(AudioBookResponse::from).collect()),
