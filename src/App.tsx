d1f31dbc9b6dc272b9963b73b0189e3df486e720
Samiul Alim <xo.ot2001@gmail.com>
2025-10-25 22:31:12 +0600

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
+
+function App() {
+  return (
+    <main className="container">
+      <Button className="hover:cursor-pointer">Click me</Button>
+    </main>
+  );
+}
+
+export default App;
