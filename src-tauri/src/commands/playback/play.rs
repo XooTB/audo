bfaac802210dead2757382ed93ef6e2fdaf33338
XooTB <xo.ot2001@gmail.com>
2026-02-01 10:58:12 +0600

    fix: temp state

--- src-tauri/src/commands/playback/play.rs
+++ src-tauri/src/commands/playback/play.rs
@@ -17,22 +17,22 @@ pub async fn play(
 
     // Get the audio player from the state
     let mut player = audio_player.lock().unwrap();
-    
+
     // Check if the book is already loaded into the player.
     if player.current_track_path.is_some()
-    && player.current_track_path.as_ref().unwrap() == &book.file_location
+        && player.current_track_path.as_ref().unwrap() == &book.file_location
     {
-        player.play().map_err(|e| e.to_string())?;
+        player.play(0).map_err(|e| e.to_string())?;
         return Ok(());
     }
-    
+
     // Otherwise, load the book into the player.
     player
-        .change_current_track(&book.file_location)
+        .change_current_source(&book.file_location)
         .map_err(|e| e.to_string())?;
 
     // And then, play the audio.
-    player.play().map_err(|e| e.to_string())?;
+    player.play(0).map_err(|e| e.to_string())?;
 
     Ok(())
 }
@@ -41,7 +41,7 @@ pub async fn play(
 pub async fn pause(audio_player: tauri::State<'_, Mutex<AudioPlayer>>) -> Result<(), String> {
     let mut audio_player = audio_player.lock().unwrap();
     // Pause the audio
-    audio_player.pause().map_err(|e| e.to_string())?;
+    audio_player.pause(0).map_err(|e| e.to_string())?;
 
     Ok(())
 }
